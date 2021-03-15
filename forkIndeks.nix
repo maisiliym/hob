3d3acceaf8e0c84207b834ed2f9250d01eeb5443
li <@li:maisiliym.uniks>
2021-03-15 10:48:14 +0000

    @neovimFork

--- forkIndeks.nix
+++ forkIndeks.nix
@@ -1,8 +1,19 @@
 getFleik:
 {
+  neovim = {
+    maisiliym = {
+      mein = getFleik {
+        url = file:///git/github.com/neovim/neovim;
+        type = "git";
+        ref = "maisiliym/dev-1";
+        flake = false;
+      };
+    };
+  };
+
   nvim-bufferline-lua = {
     maisiliym = {
-      dev = {
+      dev = getFleik {
         url = file:///git/github.com/akinsho/nvim-bufferline.lua;
         type = "git";
         ref = "maisiliym/dev";
