449082d4903d9b32fcbcc945257bc28e6c2609b2
li <@li:maisiliym.uniks>
2021-04-20 04:11:22 +0000

    (init (skiim hob))

---
+++ hob.scm
@@ -0,0 +1,22 @@
+(define-module (hob)
+  #:use-module (giiks)
+  #:use-module (oop goops)
+  #:export ())
+
+(define-class Hob ()
+ (spoks #:accessor Hob.spoks
+        #:init-form (make-hash-table))
+
+(define-class Spok ()
+ (links #:accessor Spok.links
+        #:init-keyword #:links)
+ (hac #:accessor Spok.hac
+      #:init-keyword #:hac))
+
+(define-class GitSpok (Spok)
+ (commit #:accessor Spok.commit
+         #:init-keyword #:commit))
+
+(define hob
+ (let ((empty-hob (make Hob)))
+  () ) )
